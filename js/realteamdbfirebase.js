2b81e089995c1109d9c1026d4a493e9843e8858f
Anna Arthdi Putra <AERDY4@PID-PROJECT-AERDY4.local>
2023-03-19 13:22:19 +0700

    reverse wisher

---
+++ js/realteamdbfirebase.js
@@ -0,0 +1,52 @@
+// Initialize Firebase
+ const firebaseConfig = {
+    apiKey: "AIzaSyAEQIoXbPvHsYQrAlnGL_vrBKnf_KkISzA",
+    authDomain: "necis-research.firebaseapp.com",
+    databaseURL: "https://necis-research.firebaseio.com",
+    projectId: "necis-research",
+    storageBucket: "necis-research.appspot.com",
+    messagingSenderId: "1017923930975",
+    appId: "1:1017923930975:web:c0d21999211049ce0b04ea"
+  };
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+loaddata()
+function pushData(){
+  var name = document.getElementById("fname").value;
+  var message = document.getElementById("message").value;
+  var dataRef = database.ref('/nikahincom').push();
+  dataRef.set({
+    name: name,
+    message: message
+  });
+  ClearFields();
+  loaddata();
+}
+
+
+function loaddata(){
+    var itemwish = [];
+    database.ref('/nikahincom').orderByChild("date").on('value', function(snapshot){
+    snapshot.forEach(function(data){
+      itemwish.push(
+                      '<div class="testimony-slide active text-center">'+
+                        '<figure>'+
+                          '<img src="images/bride.jpg" alt="user">'+
+                        '</figure>'+
+                      `<span id="idnamewish">${data.val().name}, via <a href="#" class="twitter">Twitter</a></span>`+
+                      `<small id="idmesswish" class="block">"${data.val().message}"</small>`+
+                    '</div>');
+      
+    });
+    document.getElementById("itemwish").innerHTML = itemwish.reverse();
+    console.log(Object.keys(snapshot.val()));
+  });
+}
+
+
+function ClearFields() {
+     document.getElementById("fname").value = "";
+     document.getElementById("message").value = "";
+}
